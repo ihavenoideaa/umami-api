d06bf2fcb74ad9d33c2b68f7e4cbd6a94f1afd32
Ihavenoideaa <15219599524@163.com>
2023-04-08 19:21:10 +0800

    数据处理

--- api/getEvents.js
+++ api/getEvents.js
@@ -12,7 +12,24 @@ module.exports = async (req, res) => {
         unit: "year"       // 以年为间隔，相同的项在不同间隔触发会分开显示
       }
     );
-    return res.send(JSON.stringify(eventsview));
+  
+    // 处理 eventsview 中的数据 
+  const result = {}; 
+  for(let item of eventsview) 
+  { 
+	  if(result[item.x]) 
+	  { 
+		  result[item.x] += item.y; 
+	  } 
+	  else 
+	  { 
+		  result[item.x] = item.y; 
+	  } 
+  } 
+  const sortedData = Object.entries(result).sort((a, b) => b[1] - a[1]);
+  return res.send(JSON.stringify(sortedData.map(item => ({x: item[0], y: item[1]}))));
+
+    //return res.send(JSON.stringify(eventsview));
   } catch (error) {
     console.error(error);
     return res.status(500).send("Oops! check the vercel log.");
